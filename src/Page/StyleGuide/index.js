58ba1c52516d460da1ba2ffdf1f1ba37d5244f4f
hieptv <hieptv@arrowhitech.net>
2022-09-18 17:36:14 +0700

    make product item

--- src/Page/StyleGuide/index.js
+++ src/Page/StyleGuide/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useCallback, useEffect, useRef, useState } from 'react'
 import Banner from '../../component/Banner'
 import Header from '../../component/headers/Headers'
 import Item_Slide from '../../component/Item_Slide_Shop_By_Brand/Item_Slide'
@@ -7,19 +7,29 @@ import ProductCards from '../../component/prosuctCard/ProductCard'
 import Big_Banner from "../../component/Banner_Category_Products/Banner_Category_Products"
 
 function StyleGuide() {
+  const [height, setHeight] = useState(0);
+  const handleScroll = () => {
+    let scrollTop = window.scrollY;
+    setHeight(scrollTop)
+  }
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    }
+  }, [])
   return (
     <>
-      <h3>StyleGuide</h3>
-      <div>
-        <div className='session1'>
-          <Header />
-          <Banner />
-        </div>
-        <ProductCards />
-        <Item_Slide />
-        <Big_Banner />
-        <ProductItemView />
-      </div>
+    <div>
+    <div className='session1'>
+        <Header heightsPage={height} />
+        <Banner/>
+       </div>
+      <ProductCards />
+      <Item_Slide />
+      <Big_Banner />
+      <ProductItemView/>
+    </div>
     </>
   )
 }
