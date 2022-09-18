fadfb366f27639378ab42d63c0af76b6b80dbad8
HanhDHT <doanhonghanh.1999@gmail.com>
2022-09-18 11:08:18 +0700

    add layout component

---
+++ src/Page/About/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Layout from '../../component/Layout'
+
+function About() {
+  return (
+    <Layout>
+      <div>AboutPage</div>
+    </Layout>
+    
+  )
+}
+
+export default About
---
+++ src/Page/StyleGuide/index.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import Banner from '../../component/Banner'
+import Header from '../../component/headers/Headers'
+import Item_Slide from '../../component/Item_Slide_Shop_By_Brand/Item_Slide'
+import ProductItemView from '../../component/ProductItemView/ProductItemView'
+import ProductCards from '../../component/prosuctCard/ProductCard'
+import Big_Banner from "../../component/Banner_Category_Products/Banner_Category_Products"
+
+function StyleGuide() {
+  return (
+    <>
+    <h3>StyleGuide</h3>
+    <div>
+    <div className='session1'>
+        <Header />
+        <Banner/>
+       </div>
+      <ProductCards />
+      <Item_Slide />
+      <Big_Banner />
+      <ProductItemView/>
+    </div>
+    </>
+  )
+}
+
+export default StyleGuide
