f16d3b4e3fafa265bce9ae4c7e1f71dd2409fba7
hoang0508 <nguyenhuyhoang05082001@gmail.com>
2022-09-18 14:58:07 +0700

    setup-gitignore

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-import './scss/style.scss';
+import logo from "./logo.svg";
+import "./App.css";
+import "./scss/style.scss";
 import { Routes, Route, Link } from "react-router-dom";
 import About from "../src/Page/About";
 import HomePage from "../src/Page/HomePage";
 import StyleGuide from "../src/Page/StyleGuide";
+import OrderDetails from "./Page/OrderDetails";
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<StyleGuide/>} />
-        <Route path="home-page" element={<HomePage/>} />
+        <Route path="/" element={<StyleGuide />} />
+        <Route path="home-page" element={<HomePage />} />
         <Route path="about" element={<About />} />
+        <Route path="/order-details" element={<OrderDetails />} />
       </Routes>
     </div>
   );
