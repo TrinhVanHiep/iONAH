32cedbf338225fa4655a69bdbacd8d92a8e3b3dc
anhtu-devfe <nguyenanhtu2204tb@gmail.com>
2022-09-18 09:43:54 +0700

    make ui big banner,footer,popup notifi,item slide

---
+++ src/Components/Product_Item_Cart/Product_Item_Cart.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function Product_Item_Cart() {
+    return (
+        <div>Product_Item_Cart</div>
+    )
+}
+import React from 'react'
+
+export default function Product_Item_Cart() {
+    return (
+        <div>
+
+        </div>
+    )
+}
