5263374224ed4dfe1cf8fd8b65ba027189899dba
hieptv <hieptv@arrowhitech.net>
2022-09-18 18:02:53 +0700

    make product image

--- src/Page/StyleGuide/index.js
+++ src/Page/StyleGuide/index.js
@@ -5,6 +5,7 @@ import Item_Slide from '../../component/Item_Slide_Shop_By_Brand/Item_Slide'
 import ProductItemView from '../../component/ProductItemView/ProductItemView'
 import ProductCards from '../../component/prosuctCard/ProductCard'
 import Big_Banner from "../../component/Banner_Category_Products/Banner_Category_Products"
+import ProductImages from '../../component/ProductImage/ProductImage'
 
 function StyleGuide() {
   const [height, setHeight] = useState(0);
@@ -25,9 +26,15 @@ function StyleGuide() {
         <Header heightsPage={height} />
         <Banner/>
        </div>
+       <div style={{margin: 20}}></div>
       <ProductCards />
+      <div style={{margin: 20}}></div>
+      <ProductImages />
+      <div style={{margin: 20}}></div>
       <Item_Slide />
+      <div style={{margin: 20}}></div>
       <Big_Banner />
+      <div style={{margin: 20}}></div>
       <ProductItemView/>
     </div>
     </>
